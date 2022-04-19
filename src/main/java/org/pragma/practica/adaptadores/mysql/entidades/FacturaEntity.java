d7a44ba2e282d9e9d05135d8002cedf2fb6b4db6
CAMILO LEAL <ingenierocamiloleal@gmail.com>
2022-04-18 20:37:06 -0500

    Creando Entidad FacturaEntity

--- src/main/java/org/pragma/practica/adaptadores/mysql/entidades/FacturaEntity.java
+++ src/main/java/org/pragma/practica/adaptadores/mysql/entidades/FacturaEntity.java
@@ -1,4 +1,23 @@
 package org.pragma.practica.adaptadores.mysql.entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "facturas")
 public class FacturaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String numeroFactura;
+    private LocalDate fecha;
+    private boolean estado;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private ClienteEntity cliente;
 }
--- src/main/java/org/pragma/practica/dominio/modelo/Factura.java
+++ src/main/java/org/pragma/practica/dominio/modelo/Factura.java
@@ -1,16 +1,15 @@
 package org.pragma.practica.dominio.modelo;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class Factura {
     private Integer id;
