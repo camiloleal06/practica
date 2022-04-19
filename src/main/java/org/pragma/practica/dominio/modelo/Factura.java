ce1ba810f5401f33d6213db0fb1a566de30896f7
CAMILO LEAL <ingenierocamiloleal@gmail.com>
2022-04-18 20:42:27 -0500

    Creando Entidad FacturaEntity

--- src/main/java/org/pragma/practica/dominio/modelo/Factura.java
+++ src/main/java/org/pragma/practica/dominio/modelo/Factura.java
@@ -1,9 +1,10 @@
 package org.pragma.practica.dominio.modelo;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Getter
